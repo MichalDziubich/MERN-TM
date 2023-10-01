e2416f9758fb80a0f7e4e2ee14ac79037a6ed1a7
Michal Dziubich <michal.dziubich@gmail.com>
2023-10-01 23:29:39 +0200

    REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -58,7 +58,7 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    await goal.remove()
+    await goal.deleteOne()
 
     res.status(200).json({ id: req.params.id })
 })
